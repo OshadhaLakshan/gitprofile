093c1bb6da7e43de128e10cee6fe77d3c1ab4a0c
Oshadha Lakshan <oshadhaen@gmail.com>
2024-07-16 10:15:45 +0530

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -20,7 +20,7 @@ const CONFIG = {
         limit: 8, // How many projects to display.
         exclude: {
           forks: false, // Forked projects will not be displayed if set to true.
-          projects: ['OshadhaLakshan/OshadhaLakshan', 'OshadhaLakshan/oshadha.github.io', 'OshadhaLakshan/gitprofile'], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+          projects: ['OshadhaLakshan/OshadhaLakshan', 'OshadhaLakshan/oshadha.github.io', 'OshadhaLakshan/gitprofile', 'OshadhaLakshan/material-ui', 'OshadhaLakshan/ui'], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
       manual: {
